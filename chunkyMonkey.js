ace8e46fc0da03fdf48639955117a621cdee1e80
Joseph Adeyemo <j_adeyemo@yahoo.com>
2021-06-01 08:54:08 -0400

    8:53AM 6/1/2021

---
+++ chunkyMonkey.js
@@ -0,0 +1,16 @@
+function chunkyMonkey(arr, size) {
+    const nested = [];
+    let count = 0;
+
+    while (count < arr.length){
+        nested.push(arr.slice(count, count += size))
+    }
+
+
+
+    return nested
+}
+
+
+console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
+console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
