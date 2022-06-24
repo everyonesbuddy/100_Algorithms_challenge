c207c3dae35387062331da8d4e6d13d78144d16d
Joseph <47672995+everyonesbuddy@users.noreply.github.com>
2022-06-24 09:36:47 -0400

    6/24 9:36Am

--- absoluteValuesSumMinization.js
+++ absoluteValuesSumMinization.js
@@ -1,22 +1,23 @@
-//my solution 
-// function absoluteValuesSumMinimization(arr) {
-//     //check if the array is even
-//     if (arr.length % 2 === 0){
-//         return arr[(arr.length / 2) + 1]
-//     } else {
-//         return arr[arr.length / 2]
-//     }
-// }
-
-//working solution
+//my solution
 function absoluteValuesSumMinimization(arr) {
-    //check if the array is even
-    if (arr.length % 2 === 0){
+    let median = arr.length / 2;
+    if (arr.length % 2 === 0) {
         return arr[(arr.length / 2) - 1]
     } else {
         return arr[Math.floor(arr.length / 2)]
     }
 }
+
+
+//working solution
+// function absoluteValuesSumMinimization(arr) {
+//     //check if the array is even
+//     if (arr.length % 2 === 0){
+//         return arr[(arr.length / 2) - 1]
+//     } else {
+//         return arr[Math.floor(arr.length / 2)]
+//     }
+// }
 
 console.log(absoluteValuesSumMinimization([2, 4, 7]));
 console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
