b20c1c69bcdb72703636d465a58c45cd8629165e
Joseph <47672995+everyonesbuddy@users.noreply.github.com>
2022-07-14 21:41:10 -0400

    7/14/2022 9:41PM

--- arrayMaxConsecutiveSum.js
+++ arrayMaxConsecutiveSum.js
@@ -1,26 +1,42 @@
 //working Solution
 
-function  arrayMaxConsecutiveSum(array, num){
+// function  arrayMaxConsecutiveSum(array, num){
+//     let sum = 0;
+//     let max = 0;
+
+//     for (let i = 0; i < num; i++){
+//         sum += array[i]
+//     }
+
+//     max = sum;
+
+//     for(let i = num; i < array.length; i++){
+//         sum -= array[i-num];
+//         sum += array[i];
+
+
+//         if(sum > max) {
+//             max = sum
+//         }
+//     }
+
+//     return sum
+// }
+
+//resvision solution
+function arrayMaxConsecutiveSum(array, num){
+    let maximumPossibleSum = 0;
     let sum = 0;
-    let max = 0;
 
-    for (let i = 0; i < num; i++){
-        sum += array[i]
+    for(let i = 0; i < array.length; i++) {
+        sum = array[i] + array[i + 1];
+
+         if(sum > maximumPossibleSum) {
+             maximumPossibleSum = sum
+         }
+
     }
-
-    max = sum;
-
-    for(let i = num; i < array.length; i++){
-        sum -= array[i-num];
-        sum += array[i];
-
-
-        if(sum > max) {
-            max = sum
-        }
-    }
-
-    return sum 
+    return maximumPossibleSum
 }
 
 console.log(arrayMaxConsecutiveSum([2,3,5,1,6], 2))
